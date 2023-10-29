afdfdb70cc60fce3fe4b6288481144cb6106cfe2
Maria <zhukovamaria9@gmail.com>
2023-10-29 12:17:45 +0300

    add conversion to metres

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@ const inputWeight = document.getElementById("weight");
 const inputHeight = document.getElementById("height");
 const submitButton = document.getElementById("button");
 
-let weight = Number(inputWeight.value);
-let height = Number(inputHeight.value);
-
 function calculateBMI() {
+  let weight = Number(inputWeight.value);
+  let heightBeforeConvertion = Number(inputHeight.value);
+  let height = heightBeforeConvertion / 100;
   const heightForBmiCalculation = Math.pow(height, 2);
   const weightForBmiCalculation = weight / heightForBmiCalculation;
-  const result = Math.round(weightForBmiCalculation * 100);
+  const result = Math.round(weightForBmiCalculation.toFixed(2));
   inputWeight.value = "";
   inputHeight.value = "";
   alert(result);
